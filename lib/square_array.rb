55e936388bf7fdf8b658216e31982812536879d6
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-04 22:09:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   counter = 0 
+  new_array = []
   while array[counter] do
-    array.to_a [counter**2]
+    array [counter**2] = new_array
     counter += 1
   end
+  puts new_array
 end
